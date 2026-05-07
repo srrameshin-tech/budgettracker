3a59e001c1fe4f98f4a8d20868e13e3a8bd0600d
srrameshin-tech <srrameshin@gmail.com>
2026-05-08 05:19:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+create table entries (
 
+id bigint generated always as identity primary key,
+
+username text,
+
+date text,
+
+type text,
+
+category text,
+
+description text,
+
+amount numeric
+
+);
